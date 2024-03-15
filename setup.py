0f213cfd68a94f3eece50576a5984a99f8108801
Niels Lauritzen <niels.lauritzen@disent.com>
2024-03-15 15:01:14 -0400

    0.0.30

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="dateroll",
-    version="0.0.28",
+    version="0.0.30",
     description="""dateroll makes working with dates less painful.""",
     author="Anthony Malizzio",
     author_email="anthony.malizzio@disent.com",
