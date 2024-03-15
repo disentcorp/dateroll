005bb92c3d169f4d57c6fd409d54721f36dce1bd
Niels Lauritzen <niels.lauritzen@disent.com>
2024-03-15 11:54:57 -0400

    pages build

--- pages/convert.sh
+++ pages/convert.sh
@@ -1,5 +1,14 @@
 #/bin/bash
 
+mkdir -p build/docs
+cp ../logo.png build
+if [[ $? ]]; then
+    cp github-markdown.css build    
+else
+    echo "ERROR: run this script from the pages directory."
+    exit 1
+fi
+
 pandoc  --metadata=title="Dateroll" --variable=title="" -H index_header.html -c github-markdown.css  -s -f markdown -t html ../README.md  > build/index.html
-pandoc  --metadata=title="Dateroll Docs" --variable=title="" -H index_header.html -c github-markdown.css  -s -f markdown -t html ../DOCS.md  > build/docs.html
+pandoc  --metadata=title="Dateroll Docs" --variable=title="" -H index_header.html -c github-markdown.css  -s -f markdown -t html ../DOCS.md  > build/docs/docs.html
 pandoc  --metadata=title="Dateroll Examples" --variable=title="" -H index_header.html -c github-markdown.css  -s -f markdown -t html ../EXAMPLES.md  > build/examples.html
