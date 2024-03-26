e41f43a09e92443c0f4dbf0ef62be0c72f4d7d05
amalizzio <anthony.malizzio@disent.com>
2024-03-26 10:44:44 -0400

    changes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="dateroll",
-    version="0.1.2",
+    version="0.1.3",
     description="""dateroll makes working with dates less painful.""",
     author="Anthony Malizzio",
     author_email="anthony.malizzio@disent.com",
