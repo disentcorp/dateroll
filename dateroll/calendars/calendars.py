ca43ab4227119f4118b1e01fd8b7591ce6f7b401
Niels Lauritzen <niels.lauritzen@disent.com>
2024-04-05 13:42:11 -0400

    go back to open

--- dateroll/calendars/calendars.py
+++ dateroll/calendars/calendars.py
@@ -75,7 +75,7 @@ class Drawer:
 
         if self.path.exists():
             try:
-                with self.path.open("rb") as f:
+                with open(self.path, "rb") as f:
                     self.data = pickle.load(f)
                     self.cals.db_hash = self.cals.hash
                     self.cals.db = self.data
